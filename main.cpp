460409f989bba3d12a58ede6db337a00413294be
TzuChieh <james093131@gmail.com>
2020-08-28 11:42:56 +0800

    croossoverup

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include<time.h>
 #include<math.h>
 #include<vector>
+#define MR 0.1
 using namespace std;
 class SE_Init { 
 public: 
@@ -24,6 +25,51 @@ void random_zero_or_one(vector<vector<int> > &P)
             int ran=rand()%2;
             P[i][j]=ran;
         }
+    }
+}
+void mutation(vector<int>&P,int ind,int category,vector<int>lock)//隨機選取一點做調換
+{
+    int c=rand()%ind;
+    if(lock[c]==0)
+    {
+        int a=rand()%category;
+        P[c]=a;
+    }
+}
+void crossover(vector<vector<int> > &P1,vector<vector<int> > &P2,int pop,int ind)
+{
+    int i=0;
+    while(i<pop){
+        int c1=rand()%ind;
+        float t1 = (float) rand() / (RAND_MAX + 1.0);
+        for(int k=0;k<ind;k++)
+        {
+           
+                if(k>c1)
+                {
+                    int temp=P1[i][k];
+                    P1[i][k]=P2[i+1][k];
+                    P2[i+1][k]=temp;
+                } 
+                else{
+                    
+                    P1[i][k]=P1[i][k];
+                    P2[i+1][k]=P2[i+1][k];
+                }
+            
+           
+        }
+        float x = (float) rand() / (RAND_MAX + 1.0);
+        float y = (float) rand() / (RAND_MAX + 1.0);
+        if(x<MR)
+        {
+            mutation(P[i],ind,category,lock);
+        }
+        if(y<MR)
+        {
+             mutation(P[i+1],ind,category,lock);
+        }
+         i+=2;
     }
 }
 int main(int argc, const char * argv[]) {
