1cb20322fc40b245732ce7e27a91a9332886e1d7
TzuChieh <james093131@gmail.com>
2020-08-28 12:05:05 +0800

    SE

--- main.cpp
+++ main.cpp
@@ -13,8 +13,9 @@ class SE_Init {
 public: 
 	vector<vector<int> > Searcher ;//分類
     vector<vector<int> > Good ;//分類
+    vector<vector<int> > SampleV ;//vision search產生的解
 };
-void random_zero_or_one(vector<vector<int> > &P)
+void random_zero_or_one(vector<vector<int> > &P)//隨機起始01
 {
     int a=P.size();
     int b=P[0].size();
@@ -27,49 +28,41 @@ void random_zero_or_one(vector<vector<int> > &P)
         }
     }
 }
-void mutation(vector<int>&P,int ind,int category,vector<int>lock)//隨機選取一點做調換
+void mutation(vector<int> &P,int ind)//隨機選取一點做調換
 {
-    int c=rand()%ind;
-    if(lock[c]==0)
-    {
-        int a=rand()%category;
-        P[c]=a;
-    }
+     int c=rand()%ind;
+    if(P[c]==0)
+        P[c]=1;
+    else
+        P[c]=0;
 }
-void crossover(vector<vector<int> > &P1,vector<vector<int> > &P2,int pop,int ind)
+void crossover(vector<vector<int> > &P,vector<vector<int> > P1,vector<vector<int> > P2,int P1pop,int P2pop,int ind)
 {
     int i=0;
-    while(i<pop){
-        int c1=rand()%ind;
-        float t1 = (float) rand() / (RAND_MAX + 1.0);
-        for(int k=0;k<ind;k++)
+    while(i<P1pop){
+
+        for(int j=0;j<P2pop;j++)
         {
+            int c1=rand()%ind;
+            for(int k=0;k<ind;k++)
+            {
            
                 if(k>c1)
                 {
-                    int temp=P1[i][k];
-                    P1[i][k]=P2[i+1][k];
-                    P2[i+1][k]=temp;
+                    P[i*j+j][k]=P2[i][k];
                 } 
                 else{
-                    
-                    P1[i][k]=P1[i][k];
-                    P2[i+1][k]=P2[i+1][k];
+                    P[i*j+j][k]=P1[i][k]; 
                 }
+            }
+            // float x = (float) rand() / (RAND_MAX + 1.0);
+            // if(x<MR)
+            // {
+            //     mutation(P[i],ind);
+            // }
             
-           
         }
-        float x = (float) rand() / (RAND_MAX + 1.0);
-        float y = (float) rand() / (RAND_MAX + 1.0);
-        if(x<MR)
-        {
-            mutation(P[i],ind,category,lock);
-        }
-        if(y<MR)
-        {
-             mutation(P[i+1],ind,category,lock);
-        }
-         i+=2;
+        i++;
     }
 }
 int main(int argc, const char * argv[]) {
@@ -82,10 +75,10 @@ int main(int argc, const char * argv[]) {
     SE_Init SE_ini;
     SE_ini.Searcher.resize(SE_Quan,vector<int>(Bit));//幾個searcher and length is bits
     SE_ini.Good.resize(SE_Quan*Good_Quan,vector<int>(Bit));
-    cout<<SE_ini.Searcher.size()<<' '<<endl;
-    cout<<SE_ini.Searcher[0].size()<<' '<<endl;
-    cout<<SE_ini.Good.size()<<' '<<endl;
-    cout<<SE_ini.Good[0].size()<<' '<<endl;
+    SE_ini.SampleV.resize(SE_Quan*SE_Quan*Good_Quan,vector<int>(Bit));
+    
+    cout<<SE_ini.SampleV.size()<<' '<<endl;
+    cout<<SE_ini.SampleV[0].size()<<' '<<endl;
     random_zero_or_one(SE_ini.Searcher);
     random_zero_or_one(SE_ini.Good);
     // for(int i=0;i<SE_ini.Good.size();i++)
